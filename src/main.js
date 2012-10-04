c9786bdc0fc423c2f802587064f46579940f3afa
Tim <thogg4@gmail.com>
2012-10-04 11:16:13 -0600

    style changes, and remove comma between month and year

--- src/main.js
+++ src/main.js
@@ -229,7 +229,7 @@ Kalendae.prototype = {
 		subscribe:				null,			/* object containing events to subscribe to */
 
 		columnHeaderLength:		2,				/* number of characters to show in the column headers */
-		titleFormat:			'MMMM, YYYY',	/* format mask for month titles. See momentjs.com for rules */
+		titleFormat:			'MMMM YYYY',	/* format mask for month titles. See momentjs.com for rules */
 		dayNumberFormat:		'D',			/* format mask for individual days */
 		dayAttributeFormat:		'YYYY-MM-DD',	/* format mask for the data-date attribute set on every span */
 		parseSplitDelimiter:	/,\s*|\s+-\s+/,	/* regex to use for splitting multiple dates from a passed string */
